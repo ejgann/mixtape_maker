f123dc1dce3aae6abef6fb4f5a9bcad093370d9d
Emily Gann <aspentree79@gmail.com>
2020-06-27 14:56:40 -0400

    created sessions controller and build login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  
+  root 'sessions#home'
+
   # signup
   get '/signup', to: 'users#new'
   
 
   # login
-
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   # logout
+  delete '/logout'
   
   
   resources :users
